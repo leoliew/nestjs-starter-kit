f063a7ff237f3680ba4a60d06bb4da7d1f0dfb4c
leo <zmliu0077@gmail.com>
2023-07-25 11:34:03 +0800

    add prod env settign

--- config/production.js
+++ config/production.js
@@ -1,3 +1,24 @@
 module.exports = {
   port: process.env.PORT || 3000,
+  mongodb: {
+    debug: true,
+    connections: [
+      {
+        name: 'main',
+        mongodbURI: process.env.MAIN_MONGODB_URI,
+        options: {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        },
+      },
+      {
+        name: 'logs',
+        mongodbURI: process.env.LOGS_MONGODB_URI,
+        options: {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        },
+      },
+    ],
+  },
 };
