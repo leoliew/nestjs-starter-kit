3e97302a6b3f99b8c1f661f3523c1bfed5f9c5fd
Leo Liu <zmliu0077@gmail.com>
2023-08-02 09:07:03 +0800

    delte comment

--- src/lib/bcrypt.ts
+++ src/lib/bcrypt.ts
@@ -7,8 +7,6 @@ export default class Bcrypt {
   }
 
   static decrypt(ciphertext: string): string {
-    // console.log(ciphertext);
-    // console.log('========');
     const bytes = crypto.AES.decrypt(ciphertext, secretKey);
     const decryptStr = bytes.toString(crypto.enc.Utf8);
     console.log(decryptStr);
